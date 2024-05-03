c419c954b6ccb2e030c05e631a0f55b858f904d9
anntingg <162669802+anntingg@users.noreply.github.com>
2024-05-03 16:56:52 +0800

    0503-v3

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(function () {
+    $("#btn-top").click(function () {
+        $("html, body").animate({ scrollTop: 0 }, "fast", "swing");
+    });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $("#btn-top").stop().fadeTo("", 1);
+        } else {
+            $("#btn-top").stop().fadeTo("", 0);
+        }
+    });
+})
+
+
