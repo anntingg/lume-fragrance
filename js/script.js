28e74ca8e6e28801ff51404484ef51fe48e145f1
anntingg <162669802+anntingg@users.noreply.github.com>
2024-05-13 00:58:49 +0800

    0512-v1

--- js/script.js
+++ js/script.js
@@ -20,7 +20,6 @@ $(function () {
         // 取出 footer 和視窗底部位置
         let footerPos = $("footer").offset().top;
         let currentBottomPos = $(this).scrollTop() + window.innerHeight;
-        // console.log(footerPos, windowBottomPos);
 
         // 當視窗底部抵達 footer 時，Top 按鈕新增 class="position" 覆蓋原本定位
         if (currentBottomPos >= (footerPos)) {
@@ -38,9 +37,6 @@ $(function () {
             let linkSection = $(this).attr("href"); //取得錨點連結的屬性值
             let sectionTop = ($(linkSection).offset().top) -1;  // 抓上面屬性值的座標中 Top 位置
             let sectionBottom = sectionTop + $(linkSection).outerHeight();
-
-            // console.log(currentTopPos);
-            // console.log($("#facial-mask").offset().top);
 
             if (currentTopPos >= sectionTop && currentTopPos <= sectionBottom) {
                 $(anchorProduct).removeClass("active");
